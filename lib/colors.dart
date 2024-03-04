7da8508602a767250266915815def9af4c3b3cb1
austin-yoshino <austin@joynchat.com>
2024-03-04 12:53:39 -1000

    colors

--- lib/colors.dart
+++ lib/colors.dart
@@ -5,7 +5,6 @@ class AppColors {
   static const Color darkGrey = Color(0xFF1E1E1E);
   static const Color gold = Color(0xFFe5d57e);
   static const Color joynBlue = Color(0xFF2798ff);
-  static const Color joynPurple = Color(0xFF9863f8);
   static const Color phenomBlue = Color(0xFF900aff0);
   static const Color phenomBlacj = Color(0xFF231F20);
   static const Color phenomGreen = Color(0xFFA4FD02);
